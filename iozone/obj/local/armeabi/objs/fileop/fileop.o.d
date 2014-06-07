b177b9b1130460e2644a511d829509a88ad81145
Jeff Fisher <jfisher@blackpearlsystems.com>
2014-06-06 18:38:43 -0700

    Add iozone to benchmark tests.

---
+++ iozone/obj/local/armeabi/objs/fileop/fileop.o.d
@@ -0,0 +1,157 @@
+obj/local/armeabi/objs/fileop/fileop.o: jni/fileop.c \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/dirent.h
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/dirent.h:
---
+++ iozone/obj/local/armeabi/objs/iozone/iozone.o.d
@@ -0,0 +1,259 @@
+obj/local/armeabi/objs/iozone/iozone.o: jni/iozone.c \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/errno.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/wait.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/resource.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/resource.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/resource.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/wait.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/mman.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/mman.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/socket.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/socket.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/socket.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/sockios.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/sockios.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/uio.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netinet/in.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/in.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/byteorder.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/in6.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/ipv6.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netinet/in6.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/arpa/inet.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/times.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/times.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/file.h \
+ /home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netdb.h
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/wait.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/resource.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/resource.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/resource.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/wait.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/mman.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/mman.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/socket.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/socket.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/socket.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/sockios.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/sockios.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/uio.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netinet/in.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/in.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/asm/byteorder.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/in6.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/ipv6.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netinet/in6.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/arpa/inet.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/times.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/linux/times.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/file.h:
+
+/home/jfisher/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/netdb.h:
